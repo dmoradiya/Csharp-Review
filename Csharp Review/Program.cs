2189d3441c3be0d98d162cc28e40da366fb51da4
dhmec <dharmoradiya@gmail.com>
2020-10-19 12:45:15 -0600

    Changed output in program.cs

--- Csharp Review/Program.cs
+++ Csharp Review/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Csharp_Review
+namespace CSharpReview_4Point2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! What a nice day!");
         }
     }
 }

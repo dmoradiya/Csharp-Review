11fd2f2dac93db2b71c66270f559605fa45d250b
dhmec <dharmoradiya@gmail.com>
2020-10-19 12:55:10 -0600

    "Dangerious code" that throws exceptions; switch statement

--- Csharp Review/Program.cs
+++ Csharp Review/Program.cs
@@ -57,6 +57,35 @@ namespace CSharpReview_4Point2
                 Console.WriteLine($"Hello {name}! What a nice day today!");
             }
 
+            int userInput;
+            string output;
+
+            Console.Write("Please enter a number: ");
+
+            // int.Parse is "dangerous" because it can throw an exception.
+            userInput = int.Parse(Console.ReadLine());
+
+            // Switch works with discrete values, not ranges like if allows, but some people find it more readable.
+            // Anything* you can do with a switch, you can do with an if/else.
+            // * Falling through cases is questionable with if/else as you need some other logic, but it's questionable whether
+            // that should be done anyways.
+
+            switch (userInput % 2)
+            {
+                case 0:
+                    output = "even";
+                    break;
+                case 1:
+                    output = "odd";
+                    break;
+                default:
+                    output = "error";
+                    break;
+            }
+
+            Console.WriteLine($"The number entered is {userInput}, it is {output}.");
+
+
 
             // Exit point of programs in C#.
         }

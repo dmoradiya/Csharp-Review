95a62516d38a8562d384567fea3eaa70eb823568
dhmec <dharmoradiya@gmail.com>
2020-10-19 12:51:15 -0600

    Decisions ; whitespace filtering

--- Csharp Review/Program.cs
+++ Csharp Review/Program.cs
@@ -25,11 +25,38 @@ namespace CSharpReview_4Point2
 
             string name;
 
-            // Input from the console.
-            name = Console.ReadLine();
+            // Write a prompt.
+            // Write will keep the cursor on the same line, WriteLine will not.
+            Console.Write("Please enter your name: ");
 
-            // Output to the console.
-            Console.WriteLine("Hello World! What a nice day today!");
+            // Input from the console.
+            // Trim() will remove all leading and trailing whitespace (newlines, spaces, tabs, etc).
+            name = Console.ReadLine().Trim();
+
+            // Condition is an expression that evaluates to either true or false. 
+            // True runs the "if" side, false runs the "else" side.
+
+            // Roughly equivalent to name == "", except it also treats null as empty, and any whitespace.
+            // Whitespace should be already trimmed, but the extra layer doesn't necessarily hurt.
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Invalid Input: Please enter a name.");
+            }
+            else if (name == "Admin")
+            {
+                Console.WriteLine("Administrative access granted.");
+            }
+            else
+            {
+                // Output to the console.
+                // Concatenation.
+                Console.WriteLine("Hello " + name + "! What a nice day today!");
+                // Parameterized Output.
+                Console.WriteLine("Hello {0}! What a nice day today!", name);
+                // String Interpolation.
+                Console.WriteLine($"Hello {name}! What a nice day today!");
+            }
+
 
             // Exit point of programs in C#.
         }

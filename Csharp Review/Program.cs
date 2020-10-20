7657df07ace364ddc329c63d42eb2416beb6901f
dhmec <dharmoradiya@gmail.com>
2020-10-20 14:09:00 -0600

    Arrays

--- Csharp Review/Program.cs
+++ Csharp Review/Program.cs
@@ -103,13 +103,34 @@ namespace CSharpReview_4Point2
 
 
             // Post-Test Loop. Always runs at least once, even if the condition is false.
-            int z = 1; // Initialize
+              int z = 1; // Initialize
             do
             {
                 Console.WriteLine(z); // Process
                 z++; // Modify
             }
             while (z <= userInput); // Test
+
+            static void Main(string[] args)
+                
+
+
+
+            // Arrays in C# are fixed-length collections of items.
+
+            // Initialize an array of length 5.
+            // The first element will have index 0, second index 1, up to fifth index 4.
+            int[] intArray = new int[] { 5, 3, 10, 12, 42 };
+
+            // If we want to set it, just like most other languages, we can set an element by assigning an index.
+            intArray[3] = 10;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+
 
 
 
